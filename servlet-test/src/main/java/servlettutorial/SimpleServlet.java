b06c6f211012a577468fded33eee2692948baa6f
romzan <roma.grigorev.2015@inbox.ru>
2022-02-07 17:56:09 +0400

    added logging

--- servlet-test/src/main/java/servlettutorial/SimpleServlet.java
+++ servlet-test/src/main/java/servlettutorial/SimpleServlet.java
@@ -1,18 +1,24 @@
 package servlettutorial;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+@Slf4j
 public class SimpleServlet extends HttpServlet {
 
-    private String message;
 
     public void init() throws ServletException {
-        message = "This is simple servlet message";
+        log.info("Servlet initialized");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,8 +28,9 @@ public class SimpleServlet extends HttpServlet {
         messageWriter.println("{\"hello\":\"dfdf\"}");
     }
 
-    public void destroy() {
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        bufferedReader.lines().forEach(System.out::println);
     }
-
 }
